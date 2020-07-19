1368f9baa3905bd9c78deb6db9c90faf9e97c25a
gabrielbaron16 <gabrielbu161@gmail.com>
2020-07-19 17:50:32 -0400

    Se Eliminó el Módulo de Categorías para Implementarlo en otro Microservicio

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 import AuthRouter from './Auth';
 import UserRouter from './Users';
 import PrivilegeRouter from './Privilege';
-import CategoryRouter from './Category'
 import DepartmenRouter from './Departments'
 
 // Init router and path
@@ -12,7 +11,6 @@ const router = Router();
 router.use('/user', UserRouter);
 router.use('/auth', AuthRouter);
 router.use('/privilege', PrivilegeRouter);
-router.use('/category', CategoryRouter);
 router.use('/department',  DepartmenRouter)
 
 // Export the base-router
--- src/shared/joi/Category.ts
+++
@@ -1,6 +0,0 @@
-import Joi from '@hapi/joi';
-
-export const createCategorySchema = Joi.object().keys({
-    name: Joi.string().required(),
-    description: Joi.string().required()
-});
