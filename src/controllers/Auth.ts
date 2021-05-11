862fe4f10e1724d803a3ade2ea07c37fc7913a79
Rommel Liendo <cloudyrl1@gmail.com>
2021-05-10 22:07:04 -0400

    feat(auth))

--- src/controllers/Auth.ts
+++ src/controllers/Auth.ts
@@ -1,46 +1,79 @@
-import { Request , Response , NextFunction } from 'express';
-import { omit, get, has } from 'lodash'
-import bcrypt from 'bcryptjs'
-import moment from 'moment'
+import { Request, Response, NextFunction } from "express";
+import { omit, get, has } from "lodash";
+import bcrypt from "bcryptjs";
+import moment from "moment";
 
-import { handleSuccess } from '@helpers/succesHandler'
-import { ErrorHandler } from '@helpers/ErrorHandler'
-import { jwtSign,jwtVerify } from '@helpers/jwt'
-import { findUserSvc } from '@services/user'
+import { handleSuccess } from "@helpers/succesHandler";
+import { ErrorHandler } from "@helpers/ErrorHandler";
+import { jwtSign, jwtVerify } from "@helpers/jwt";
+import { findUserSvc } from "@services/user";
 
-export const login = async (req : Request , res : Response , next: NextFunction) =>{
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { username, password } = req.body;
-  try{
-    const data = await findUserSvc({username});
-    if (!data) throw new ErrorHandler(400, 'WRONG_USER_PASSWORD');
+  try {
+    const data = await findUserSvc({ username });
+    if (!data) throw new ErrorHandler(400, "WRONG_USER_PASSWORD");
     const hashPass = data.password;
     const valid = await bcrypt.compare(password, hashPass);
-    if (!valid) throw new ErrorHandler(400, 'WRONG_USER_PASSWORD');
+    if (!valid) throw new ErrorHandler(400, "WRONG_USER_PASSWORD");
     const response = {
-      id : data.id,
-      username : data.username,
+      id: data.id,
+      username: data.username,
       email: data.email,
-      name : data.name,
-      last_name : data.last_name,
-      rol : data.rol,
-      department: data.department
-    }
-    const jwtInfo = await jwtSign(response)
-    handleSuccess(201, 'LOGIN SUCCESS', res, next, { response, token: jwtInfo });
-  }catch (e){
+      name: data.name,
+      last_name: data.last_name,
+      rol: data.rol,
+      department: data.department,
+    };
+    const jwtInfo = await jwtSign(response);
+    handleSuccess(201, "LOGIN SUCCESS", res, next, {
+      response,
+      token: jwtInfo,
+    });
+  } catch (e) {
     next(e);
   }
-}
+};
 
-export const check = async (req : Request , res : Response , next: NextFunction) =>{
-  try{
+export const check = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const apitoken = get(req, "headers.x-api-token", "");
     const { authorization } = req.headers;
-    if (!authorization) throw new ErrorHandler(401, 'UNAUTHORIZED');
-    const token: string = authorization.split(' ')[1];
-    const tokenPayload: any = await jwtVerify(token);
-    if (!has(tokenPayload, 'exp') || moment().unix() >= get(tokenPayload, 'exp')) throw new ErrorHandler(401, 'UNAUTHORIZED');
-    handleSuccess(201, 'LOGIN SUCCESS', res, next, omit(tokenPayload, ['iat', 'exp', 'aud', 'iss']));
-  }catch (e){
+    if (!authorization && (!apitoken || apitoken !== process.env.API_TOKEN))
+      throw new ErrorHandler(401, "UNAUTHORIZED");
+    if (!apitoken && authorization) {
+      const token: string = authorization.split(" ")[1];
+      const tokenPayload: any = await jwtVerify(token);
+      if (
+        !has(tokenPayload, "exp") ||
+        moment().unix() >= get(tokenPayload, "exp")
+      )
+        throw new ErrorHandler(401, "UNAUTHORIZED");
+      handleSuccess(
+        201,
+        "LOGIN SUCCESS",
+        res,
+        next,
+        omit(tokenPayload, ["iat", "exp", "aud", "iss"])
+      );
+    } else {
+      handleSuccess(
+        201,
+        "LOGIN SUCCESS",
+        res,
+        next,
+        {}
+      );
+    }
+  } catch (e) {
     next(e);
   }
-}
+};
